d59fce28312743a13de8a7c669530ca54d2b944e
2Cuby2 <clement.deberdt@student-cs.fr>
2023-06-17 18:22:11 +0200

    feat: improve status bar and nav bar management

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as NavigationBar from 'expo-navigation-bar';
+import { StatusBar } from 'expo-status-bar';
 import {  NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -16,25 +18,31 @@ const Stack = createNativeStackNavigator();
 
 
 const App = () => {
+    NavigationBar.setBackgroundColorAsync('#3346A6');
+    NavigationBar.setVisibilityAsync('visible');
+
     return (
-        <GameManagerProvider>
-            <TimerManagerProvider>
-                <NavigationContainer theme={DefaultTheme}>
-                    <Stack.Navigator
-                        screenOptions={{
-                            title: 'MineSweeper',
-                            headerBackVisible: false,
-                            headerStyle: headerStyle.header,
-                            headerTitleStyle: headerStyle.headerTitle,
-                            animation: 'none',
-                        }}
-                    >
-                        <Stack.Screen name='Settings' component={Settings} />
-                        <Stack.Screen name='Game' component={Game} options={{ headerRight: HeaderRight }} />
-                    </Stack.Navigator>
-                </NavigationContainer>
-            </TimerManagerProvider>
-        </GameManagerProvider>
+        <>
+            <StatusBar style="light" />
+            <GameManagerProvider>
+                <TimerManagerProvider>
+                    <NavigationContainer theme={DefaultTheme}>
+                        <Stack.Navigator
+                            screenOptions={{
+                                title: 'MineSweeper',
+                                headerBackVisible: false,
+                                headerStyle: headerStyle.header,
+                                headerTitleStyle: headerStyle.headerTitle,
+                                animation: 'none',
+                            }}
+                        >
+                            <Stack.Screen name='Settings' component={Settings} />
+                            <Stack.Screen name='Game' component={Game} options={{ headerRight: HeaderRight }} />
+                        </Stack.Navigator>
+                    </NavigationContainer>
+                </TimerManagerProvider>
+            </GameManagerProvider>
+        </>
     );
 }
 
